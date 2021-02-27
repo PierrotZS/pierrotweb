6d6e44abeaa0e3cd976dbd910e82ce8f475795d2
PierrotZS <takimiyamizu@gmail.com>
2021-02-27 20:39:55 +0700

    change icons and footrt

--- pierrotwebsite/settings.py
+++ pierrotwebsite/settings.py
@@ -18,9 +18,9 @@ BASE_DIR: Union[Path, Any] = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_$qgno#o5mzffe!wnrkpon*mjphj%or0qa=l@^od-^*emz)8fd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://pierrotweb.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -79,7 +79,6 @@ DATABASES = {
         'PORT' : ''
     }
 }
-DATABASES['default'] = dj_database_url.config(default='postgres://crxzagzydekjff:10fb3ff081ec0d3bd9d91e35cd062d6b73db31942c7b362973077a35c6d90a5b@ec2-54-146-73-98.compute-1.amazonaws.com:5432/d7fjj939sag221')
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
